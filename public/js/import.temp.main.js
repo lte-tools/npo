0c33369d326fa8204e2e7c6e93dad8a89ebcbebd
qyy213 <letscode@163.com>
2014-11-26 09:50:53 +0800

    fix  file name error
    fix  file name error

--- public/js/import.temp.main.js
+++ public/js/import.temp.main.js
@@ -2,7 +2,7 @@ require(['requirejs.config'], function () {
   'use strict';
   require(['jquery', 'bootstrap', 'cookie', 'uploadify'], function ($) {
     $(function () {
-      require(['view/import.temp.main'], function (Template_list) {
+      require(['view/import.temp'], function (Template_list) {
         Template_list.init();
       });
     });
